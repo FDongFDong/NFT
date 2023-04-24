75216916a6a379b6cd3cdef816060dbeeb9098bd
FDongFDong <abnormal5626@naver.com>
2023-04-24 22:52:32 +0900

    Fix cypto 삭제

--- lecture/hardhat.config.ts
+++ lecture/hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
-const ALCHEMY_API_KEY = '';
-const PRIVATE_KEY = '';
+const ALCHEMY_API_KEY = 'I7dn3danb00dfgjxCfVNfmDOcwXMwLbY';
+const PRIVATE_KEY =
+  'b43112fd82593f95dea3ba1a25eed28a6a75d6763677a42560b5d7815fea7977';
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
   networks: {
